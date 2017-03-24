10e2c9c0dd6a87d3bdc01cca4e8869a150d42247
Christiane Manfo <cmanfo@hs-bremen.de>
2017-03-24 13:00:47 +0100

    application zum file lesen

---
+++ read.js
@@ -0,0 +1,10 @@
+const fs = require ('fs');
+
+fs.readFile('./readme.md', (err, contents) => {
+  if (err) {
+    console.error(err);
+
+    return;
+  }
+  console.log(contents.toString('utf-8'));
+});
